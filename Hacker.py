28a0179fdd4d9691a75de280ddaca910ad55c383
ShauneLDLCL <165115202+ShauneLDLCL@users.noreply.github.com>
2025-10-14 12:38:02 +1030

    Add Hacker class with name, crypto_token, and rig attributes.

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,10 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    def __init__(self, name):
+        self.__name = name
+        self.__crypto_token = 1
+        self.__rig = False
+
