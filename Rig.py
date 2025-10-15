d6fe67cef8f9b3cd9fa7121efd2711bd2660eb82
ShauneLDLCL <165115202+ShauneLDLCL@users.noreply.github.com>
2025-10-15 23:07:20 +1030

    Create the Rig class and initialise private attributes for name, damage counter, broken state, storage, data spikes, removable drive, and upgrade level within the constructor.

--- Rig.py
+++ Rig.py
@@ -6,3 +6,15 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Rig:
+    def __init__(self, name, storage, removable_drive):
+        self.__name = name
+        self.__damage_counter = 0
+        self.__broken_state = False
+        self.__storage = storage
+        self.__data_spikes = 2
+        self.__removable_drive = removable_drive
+        self.__upgrade_level = 0
+
+
