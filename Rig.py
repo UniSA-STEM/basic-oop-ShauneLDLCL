2832dc62c2ef1537b5ccb4a5613c881ecefaf6f0
ShauneLDLCL <165115202+ShauneLDLCL@users.noreply.github.com>
2025-10-15 23:12:03 +1030

    Add Getters (Accessors) methods for name and storage.

--- Rig.py
+++ Rig.py
@@ -8,13 +8,19 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
 class Rig:
-    def __init__(self, name, storage, removable_drive):
+    def __init__(self, name, storage):
         self.__name = name
         self.__damage_counter = 0
         self.__broken_state = False
         self.__storage = storage
         self.__data_spikes = 2
-        self.__removable_drive = removable_drive
+        self.__removable_drive = 1
         self.__upgrade_level = 0
+
+    def get_name(self):
+        return self.__name
+    def get_storage(self):
+        return self.__storage
+
 
 
