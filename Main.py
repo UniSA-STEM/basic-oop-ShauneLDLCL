72d37b2028140b117222a6712378643111ae457d
ShauneLDLCL <165115202+ShauneLDLCL@users.noreply.github.com>
2025-10-17 22:25:23 +1030

    Instantiate hacker and rig objects and test the initial hacker state before rig activation.

--- Main.py
+++ Main.py
@@ -6,3 +6,23 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from Hacker import Hacker
+from Rig import Rig
+
+# 1. Instantiate the Hacker and Rig objects.
+print("*---- HACKER AND RIG INITIALISATION ----*")
+hacker = Hacker("AnonShortforAnonymous")
+rig = Rig("pc_pc(politcallycorrect_pc)")
+print(hacker.get_name())
+print(rig.get_name())
+print(20*"___")
+
+# 2. Display Initial Hacker state before rig activation.
+print("*---- INITIAL STATUS ----*")
+print("Hacker Name: ", hacker.get_name())
+print("CryptoTokens: ", hacker.get_crypto_tokens())
+print("Rig Activated: ", hacker.get_rig())
+print("Trace Level: ", hacker.get_trace_level())
+
+
