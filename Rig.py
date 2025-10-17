83aacadf1a7b678ad85a61abf489f16d711fe993
ShauneLDLCL <165115202+ShauneLDLCL@users.noreply.github.com>
2025-10-17 11:10:14 +1030

    Add storage method to add and check data spikes in rig storage and add basic repair rig method to check and import current CryptoTokens.

--- Rig.py
+++ Rig.py
@@ -7,20 +7,35 @@ Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
+from Hacker import Hacker, hacker
+
+
 class Rig:
-    def __init__(self, name, storage):
+    def __init__(self, name):
         self.__name = name
         self.__damage_counter = 0
         self.__broken_state = False
-        self.__storage = storage
+        self.__storage = []
         self.__data_spikes = 2
         self.__removable_drive = 1
         self.__upgrade_level = 0
 
     def get_name(self):
         return self.__name
-    def get_storage(self):
-        return self.__storage
 
+    def storage(self):
+        self.__storage.append(self.__data_spikes)
+        print(self.__storage)
+
+    def repair_rig(self):
+        hacker.get_crypto_tokens()
+        print("Current CryptoTokens: ",hacker.get_crypto_tokens())
+
+
+
+rig = Rig("pc_pc(politicallycorrect_pc)")
+print(rig.get_name())
+rig.storage()
+rig.repair_rig()
 
 
