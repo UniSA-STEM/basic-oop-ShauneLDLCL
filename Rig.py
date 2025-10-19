25aa759731a7822e165b4e5460a2da8152ceb313
ShauneLDLCL <165115202+ShauneLDLCL@users.noreply.github.com>
2025-10-19 19:03:53 +1030

    Implement data spike storage and launch functionality in Rig class.

--- Rig.py
+++ Rig.py
@@ -7,8 +7,6 @@ Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
-from Hacker import Hacker, hacker
-
 
 class Rig:
     def __init__(self, name):
@@ -22,20 +20,61 @@ class Rig:
 
     def get_name(self):
         return self.__name
+    def get_data_spikes(self):
+        return self.__data_spikes
 
-    def storage(self):
-        self.__storage.append(self.__data_spikes)
-        print(self.__storage)
+    def get_storage(self):
+        return list(self.__storage)
 
-    def repair_rig(self):
-        hacker.get_crypto_tokens()
-        print("Current CryptoTokens: ",hacker.get_crypto_tokens())
+    def store_data_spikes(self):
+        if self.__data_spikes > 0:
+            for i in range(self.__data_spikes):
+                self.__storage.append("Data Spike")
+            print(f"{self.__data_spikes} data spikes are stored in rig storage.")
+            self.__data_spikes = 0
+        else:
+            print("No data spikes available to store.")
+
+    def check_storage(self):
+        return "Data Spike" in self.__storage
+
+    def launch_data_spikes(self):
+        if "Data Spike" in self.__storage:
+            self.__storage.remove("Data Spike")
+            print("--- DATA SPIKE LAUNCHED FROM STORAGE ---")
+            print(f"--- REMAINING SPIKES IN STORAGE: {self.__storage.count('Data Spike')} ---")
+        elif self.__data_spikes > 0:
+            self.__data_spikes -= 1
+            print("*** DATA SPIKE LAUNCHED FROM RIG ***")
+            print(f"--- REMAINING SPIKES ATTACHED TO RIG: {self.__data_spikes}")
+        else:
+            print("~~~ NO DATA SPIKES AVAILABLE!!! ~~~")
 
 
 
-rig = Rig("pc_pc(politicallycorrect_pc)")
-print(rig.get_name())
-rig.storage()
-rig.repair_rig()
+
+
+    # def launch_data_spikes(self):
+
+
+    # def repair_rig(self):
+    #     hacker.get_crypto_tokens()
+    #     print("Current CryptoTokens: ",hacker.get_crypto_tokens())
+
+
+
+# rig = Rig("pc_pc(politicallycorrect_pc)")
+# print(rig.get_name())
+# rig.store_data_spikes()
+# print(rig.check_storage())
+# print("---")
+# rig.launch_data_spikes()
+# print(rig.check_storage())
+# rig.launch_data_spikes()
+# print(rig.check_storage())
+# rig.launch_data_spikes()
+# print(rig.check_storage())
+
+# # rig.repair_rig()
 
 
